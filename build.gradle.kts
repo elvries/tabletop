5437b0b379cbc544103ec6447795b302f95c15c4
elvries <elvries@hotmail.com>
2023-09-24 13:18:08 +0200

    Attempt to get build fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,15 @@
-import org.jetbrains.compose.compose
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
+//    kotlin("jvm")
     kotlin("multiplatform")
+//    kotlin("android")
+//    id("com.android.application")
+//    id("com.android.library")
     id("org.jetbrains.compose")
+    kotlin("plugin.serialization")
 }
 
 group "com.freeze"
@@ -14,11 +19,36 @@ repositories {
     google()
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    mavenLocal()
+    maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
 }
+
+
+/*
+val copyJsResources = tasks.create("copyJsResourcesWorkaround", Copy::class.java) {
+    from(project(":shared").file("src/commonMain/resources"))
+    into("build/processedResources/js/main")
+}
+
+val copyWasmResources = tasks.create("copyWasmResourcesWorkaround", Copy::class.java) {
+    from(project(":shared").file("src/commonMain/resources"))
+    into("build/processedResources/wasm/main")
+}
+
+afterEvaluate {
+    project.tasks.getByName("jsProcessResources").finalizedBy(copyJsResources)
+    project.tasks.getByName("wasmProcessResources").finalizedBy(copyWasmResources)
+}
+*/
 
 kotlin {
     js(IR) {
+        moduleName = "tabletop"
         browser {
+            commonWebpackConfig {
+                outputFileName = "tabletop.js"
+            }
+            /*
             testTask {
                 testLogging.showStandardStreams = true
                 useKarma {
@@ -26,14 +56,49 @@ kotlin {
                     useFirefox()
                 }
             }
+*/
         }
         binaries.executable()
     }
+
+    wasm {
+        moduleName = "tabletop"
+        browser {
+            commonWebpackConfig {
+                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).copy(
+//                    open = mapOf(
+//                        "app" to mapOf(
+//                            "name" to "google chrome canary",
+//                            "arguments" to listOf("--js-flags=--experimental-wasm-gc ")
+//                        )
+//                    ),
+                    static = (devServer?.static ?: mutableListOf()).apply {
+                        // Serve sources to debug inside browser
+                        add(project.rootDir.path)
+//                        add(project.rootDir.path + "/shared/")
+//                        add(project.rootDir.path + "/nonAndroidMain/")
+//                        add(project.rootDir.path + "/webApp/")
+                    },
+                )
+            }
+        }
+        binaries.executable()
+    }
+
     sourceSets {
         val jsMain by getting {
             dependencies {
                 implementation(compose.web.core)
                 implementation(compose.runtime)
+                implementation(compose.ui)
+                implementation(compose.foundation)
+                implementation(compose.material)
+                @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+                implementation(compose.material3)
+                @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+                implementation(compose.components.resources)
+                implementation("io.ktor:ktor-client-core:2.3.3")
+                implementation("io.ktor:ktor-client-js:2.3.3")
             }
         }
         val jsTest by getting {
@@ -42,4 +107,15 @@ kotlin {
             }
         }
     }
+}
+
+compose.experimental {
+    web.application {}
+}
+
+compose {
+    val composeVersion = project.property("compose.wasm.version") as String
+    kotlinCompilerPlugin.set(composeVersion)
+    val kotlinVersion = project.property("kotlin.version") as String
+    kotlinCompilerPluginArgs.add("suppressKotlinVersionCompatibilityCheck=$kotlinVersion")
 }
